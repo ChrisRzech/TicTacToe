81b3c0c429376e1e2f22c2c08917ea16c79b256f
Christopher Rzechula <christopherrzechula@gmail.com>
2022-01-18 01:10:12 -0800

    Move function definitions to a .cpp file

--- src/Packets.hpp
+++ src/Packets.hpp
@@ -7,12 +7,5 @@ struct TicTacToeMove
     sf::Int32 col;
 };
 
-sf::Packet& operator<<(sf::Packet& packet, const TicTacToeMove& move)
-{
-    return packet << move.row << move.col;
-}
-
-sf::Packet& operator>>(sf::Packet& packet, TicTacToeMove& move)
-{
-    return packet >> move.row >> move.col;
-}
+sf::Packet& operator<<(sf::Packet& packet, const TicTacToeMove& move);
+sf::Packet& operator>>(sf::Packet& packet, TicTacToeMove& move);
