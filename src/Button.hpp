f1e1d63e567e1fc2a536378a93bdb8987aca28ef
Christopher Rzechula <christopherrzechula@gmail.com>
2022-01-21 21:59:53 -0800

    Add button class

---
+++ src/Button.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "Input.hpp"
+#include <sfml/Graphics.hpp>
+
+class Button : public sf::Drawable
+{
+public:
+    Button(const std::string& text);
+    Button(sf::Vector2f position, const std::string& text);
+    
+    void setPosition(float x, float y);
+    void setPosition(const sf::Vector2f&);
+    void setPositionCenter(float x, float y);
+    void setPositionCenter(const sf::Vector2f&);
+    sf::Vector2f getPosition() const;
+    
+    bool isPressed(const Input&) const;
+    
+    void draw(sf::RenderTarget&, sf::RenderStates states) const override;
+
+private:
+    sf::Text m_text;
+    sf::RectangleShape m_box;
+};
