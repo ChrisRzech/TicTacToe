d3095640b2cb2c80f049500bb82f3abbd3664963
Christopher Rzechula <christopherrzechula@gmail.com>
2022-01-26 22:41:18 -0800

    Remove networking functions in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,58 +13,6 @@ static unsigned short PORT = 50'000;
 sf::TcpSocket socket;
 bool isHost = false;
 
-void hostServer(sf::TcpSocket& socket)
-{
-    std::cout << "Waiting for connection..." << std::endl;
-    
-    sf::TcpListener listener;
-    listener.listen(PORT);
-    listener.accept(socket);
-    
-    std::cout << "Connected" << std::endl;
-}
-
-void joinServer(sf::TcpSocket& socket)
-{
-    sf::Socket::Status status;
-    do
-    {
-        std::cout << "Enter address: ";
-        std::string address;
-        std::getline(std::cin, address);
-        
-        if(address == "localhost")
-            address = sf::IpAddress::getLocalAddress().toString();
-        
-        std::cout << "Attempting to connect to \"" << address << ":" << PORT << "\"..." << std::endl;
-        status = socket.connect(address, PORT, sf::seconds(5));
-        
-        if(status != sf::Socket::Status::Done)
-            std::cout << "Failure to connect" << std::endl;
-    }
-    while(status != sf::Socket::Status::Done);
-    
-    std::cout << "Connected" << std::endl;
-}
-
-bool setupSocket(sf::TcpSocket& socket)
-{
-    std::string answer;
-    do
-    {
-        std::cout << "Host server? (y/n): ";
-        std::getline(std::cin, answer);
-        
-        if(answer == "y")
-            hostServer(socket);
-        else if(answer == "n")
-            joinServer(socket);
-    }
-    while(answer != "y" && answer != "n");
-    
-    return answer == "y";
-}
-
 int main()
 {
     socket.setBlocking(false);
